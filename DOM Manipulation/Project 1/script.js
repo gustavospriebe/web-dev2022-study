61221fd45d25466b5882cfcc6a915fce123dc7c3
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2022-11-25 17:43:48 -0300

    project 1 - dom manipulation

---
+++ DOM Manipulation/Project 1/script.js
@@ -0,0 +1,28 @@
+const quoteAuthor = document.querySelector(".quote-author");
+
+const quoteText = document.querySelector(".quote-text");
+
+const button = document.querySelector(".new-quote");
+
+const quotes = async () => {
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key":
+                "9a4a5c5801mshcab81f29f643ddfp15687fjsn68a671c11be0",
+            "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+        },
+    };
+
+    const request = await fetch(
+        "https://quotes15.p.rapidapi.com/quotes/random/",
+        options
+    );
+
+    const response = await request.json();
+
+    quoteText.innerText = response.content;
+    quoteAuthor.innerText = response.originator.name;
+};
+
+button.addEventListener("click", quotes);
