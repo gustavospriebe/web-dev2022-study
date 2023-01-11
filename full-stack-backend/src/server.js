56ebb0e2bebaaefba663df4a6ce3fea413a2c18f
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-01-11 15:54:02 -0300

    connected fe and be

--- full-stack-backend/src/server.js
+++ full-stack-backend/src/server.js
@@ -1,16 +1,11 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { db, connect } from "./db.js";
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/articles/:name", async (req, res) => {
     const { name } = req.params;
-
-    const client = new MongoClient("mongodb://127.0.0.1:27017");
-    await client.connect();
-
-    const db = client.db("react-blog-db");
 
     const article = await db.collection("articles").findOne({ name });
 
@@ -24,10 +19,6 @@ app.get("/api/articles/:name", async (req, res) => {
 app.put("/api/articles/:name/upvote", async (req, res) => {
     const { name } = req.params;
 
-    const client = new MongoClient("mongodb://127.0.0.1:27017");
-    await client.connect();
-
-    const db = client.db("react-blog-db");
     await db.collection("articles").updateOne(
         { name },
         {
@@ -44,21 +35,31 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
     }
 });
 
-app.post("/api/articles/:name/comments", (req, res) => {
+app.post("/api/articles/:name/comments", async (req, res) => {
     const { name } = req.params;
     const { postedBy, text } = req.body;
-    const article = articlesInfo.find((a) => a.name === name);
+
+    await db.collection("articles").updateOne(
+        { name },
+        {
+            $push: { comments: { postedBy, text } },
+        }
+    );
+
+    const article = await db.collection("articles").findOne({ name });
     if (article) {
-        article.comments.push({ postedBy, text });
-        res.send(article.comments);
+        res.send(article);
     } else {
-        res.send("That article doesn't exists.");
+        res.sendStatus(404);
     }
 });
 
-app.listen(8000, () => {
-    console.log("Server is listenning on port 8000");
+connect(() => {
+    app.listen(8000, () => {
+        console.log("Server is listenning on port 8000");
+    });
 });
+
 
 // Estudo
 // app.post("/hello", (req, res) => {
--- full-stack-project/src/pages/ArticlePage.js
+++ full-stack-project/src/pages/ArticlePage.js
@@ -1,9 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 export default function ArticlePage() {
+    const initialState = {
+        upvote: 0,
+        comments: [],
+    };
+    const [values, setValues] = useState(initialState);
     const { articleId } = useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(
+                `/api/articles/${articleId}`
+            );
+            const articleInfo = response.data;
+            setValues(articleInfo);
+        };
+        loadArticleInfo();
+    }, []);
 
     const article = articles.find((article) => article.name === articleId);
 
@@ -14,6 +32,7 @@ export default function ArticlePage() {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This article has {values.upvote} upvote(s).</p>
             {article.content.map((paragraph, index) => (
                 <p key={index}>{paragraph}</p>
             ))}
