f0e2e78d35f494ffef6457664eeffef5efd0b783
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-01-20 15:19:32 -0300

    autocomplete challenge done

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -1,14 +1,41 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import getAutoComplete from "./api";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="w-full h-screen flex flex-col items-center bg-gray-900">
-      <input type="text" className="mt-24 mb-4" />
-      <div className="text-gray-200 flex flex-col gap-2 items-start">
-        Show results here
-      </div>
-    </div>
-  );
+    const [fruit, setFruit] = useState("");
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const query = async () => {
+            if (!fruit) {
+                return;
+            }
+            const result = await getAutoComplete(fruit);
+            setData(result);
+        };
+        query();
+    }, [fruit]);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setFruit(e.target.value);
+    };
+
+    return (
+        <div className="w-full h-screen flex flex-col items-center bg-gray-900">
+            <input
+                type="text"
+                className="mt-24 mb-4"
+                onChange={handleChange}
+                value={fruit}
+            />
+            <div className="text-gray-200 flex flex-col gap-2 items-start">
+                {!fruit && <p>Show results here</p>}
+                {fruit && data.map((fruit) => <p key={fruit}>{fruit}</p>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
